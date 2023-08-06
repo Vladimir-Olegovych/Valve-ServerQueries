c847a7b3dd2341eca75abbfab857771dce192887
Vladimir <117392112+Vladimir-Olegovych@users.noreply.github.com>
2023-08-06 11:53:06 +0400

    Delete Test.kt

--- src/main/kotlin/Test.kt
+++
@@ -1,8 +0,0 @@
-private fun main(){
-    var str = "пока и пока еще раз пока"
-    str = str.replace("пока", "bye")
-    str = str.replace("и", "and")
-    str = str.replace("еще", "another")
-    str = str.replace("раз", "chance")
-    println(str)
-}
